70386bf1ac04419dde3b3ebb625835df0bbf1f6b
Merlijn <github.y62fx@simplelogin.com>
2023-10-11 17:08:31 +0200

    fix(auth): `GoogleProvider is not a function`
    This is an object and you need to call `.default`.

--- server/api/auth.ts
+++ server/api/auth.ts
@@ -1,15 +1,17 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 //import { db } from "./schema"
 
-export default NextAuth({
-  adapter: DrizzleAdapter(database),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-    })
-  ]
-})
+// @ts-expect-error
+export default NextAuth.default({
+    adapter: DrizzleAdapter(database),
+    providers: [
+        // @ts-expect-error
+        GoogleProvider.default({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        }),
+    ],
+});
