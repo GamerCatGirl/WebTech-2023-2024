0de8356025be687a44a61473a8227d3be05dd47b
Merlijn <github.y62fx@simplelogin.com>
2023-10-20 06:56:24 +0200

    feat(database): Seeding doesn't recreate the database
    It will only fill up the database with values, and not actually
    (re)create it.

--- seed/seed.ts
+++ seed/seed.ts
@@ -1,17 +1,11 @@
-import fs from "fs";
 import { drizzle } from "drizzle-orm/better-sqlite3";
-import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 import * as schema from "../database/schema";
-import { sql } from "drizzle-orm";
 
-export const seed = async (seedData: any, removeDrizzleMigrationsTable: boolean = false) => {
+export const seed = async (seedData: any) => {
     const databaseUrl = process.env.DATABASE_URL ?? "./sqlite.db";
-    if (fs.existsSync(databaseUrl)) fs.rmSync(databaseUrl);
     const sqlite = new Database(databaseUrl);
     const database = drizzle(sqlite, { schema });
-    migrate(database, { migrationsFolder: "./drizzle" });
-    if (removeDrizzleMigrationsTable) database.run(sql`DROP TABLE __drizzle_migrations`);
     await database.transaction(async (tx) => {
         for (const tableName in seedData) {
             // @ts-ignore
--- test/API/database.ts
+++ test/API/database.ts
@@ -2,10 +2,20 @@ import { beforeEach, expect, test } from "vitest";
 import { setup, $fetch } from "@nuxt/test-utils";
 import { seedData } from "../../seed/seedData";
 import { seed } from "../../seed/seed";
+import fs from "fs";
+import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import * as schema from "../../database/schema";
 
 await setup({});
+const databaseUrl = process.env.DATABASE_URL ?? "./test/sqlite.db";
 
 beforeEach(async () => {
+    if (fs.existsSync(databaseUrl)) fs.rmSync(databaseUrl);
+    const sqlite = new Database(databaseUrl);
+    const database = drizzle(sqlite, { schema });
+    migrate(database, { migrationsFolder: "./drizzle" });
     await seed(seedData);
 });
 
