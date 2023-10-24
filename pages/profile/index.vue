4ad5860f87913293833182fc75dc42d65f1152ff
Silken <silken_heynderickx@icloud.com>
2023-10-24 11:41:38 +0200

    Adding selectors for recepies & profile layout

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,9 +1,36 @@
 <template>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
     <div>
         <lu>
             <li> My recipies </li>
-            <li> My profile  </li>
+            <li> My profile 
+                <div id="box">
+                    <h2>  My profile  </h2>
+                    <p id="importantWord">Name</p>
+                    <p id="personalInfo">example</p>
+                    <p id="importantWord">Username</p>
+                    <p id="personalInfo">example</p>
+                    <p id="importantWord">Email</p>
+                    <p id="personalInfo">example</p>
+                    <p id="importantWord">Native cuisine</p>
+                    <p id="personalInfo">example</p>
+                    <p id="importantWord">Preferences of food</p>
+                    <p id="personalInfo">example</p>
+                    <p id="importantWord">Allergies</p>
+                    <p id="personalInfo">example</p>
+                    <p id="importantWord">I don't eat...</p>
+                    <p id="personalInfo">example</p>
+                    <p id="importantWord">Info about me ...</p>
+                    <p id="personalInfo">example</p>
+                </div>
+            </li>
+            
             <li> Settings </li>
         </lu>
     </div>
 </template>
+
+
+<style>
+@import url(../../assets/css/profile.css);
+</style>
--- pages/recipies/index.vue
+++ pages/recipies/index.vue
@@ -1,3 +1,35 @@
 <template>
-    hello 
+    <select> 
+        <option>Meat and/or Fish </option>
+        <option>Vegitarian</option>
+        <option>Vegan</option>
+        <option>Pescitarian</option>
+        <option>No Fish</option>
+        <option>Haram</option>
+    </select>
+
+    <select> 
+        <option> No protein of choice </option>
+        <option> Chicken </option>
+        <option> Salmon </option>
+        <option> Beaf </option>
+        <option> .... </option>
+    </select>
+
+    <select> 
+        <option> No preference in time </option>   
+        <option> Dinner </option>
+        <option> Breakfast </option> 
+        <option> Brunch </option>
+        <option> Lunch </option>
+        <option> Snack </option>
+    </select>
+
+    <select>
+        <option> Dessert </option>
+        <option> Digestive </option>
+        <option> Apperitizers </option>
+        <option> Mean Course </option>
+        <option> Soupe </option>
+    </select>
 </template>
