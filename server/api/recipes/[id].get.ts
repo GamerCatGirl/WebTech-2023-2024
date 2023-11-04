a6df9b1a8b9bd63c64280d677200ce8ef3eb8252
Merlijn <github.y62fx@simplelogin.com>
2023-11-04 11:27:35 +0100

    refactor(database): Change recipe and image IDs to CUIDs

--- server/api/recipes/[id].get.ts
+++ server/api/recipes/[id].get.ts
@@ -1,16 +1,10 @@
-export default defineEventHandler(async (event) => {
+export default defineEventHandler((event) => {
     if (!event.context.params) {
         throw createError({
             statusCode: 400,
             statusMessage: "ID is not defined",
         });
     }
-    const id = parseInt(event.context.params.id) as number;
-    if (!Number.isInteger(id)) {
-        throw createError({
-            statusCode: 400,
-            statusMessage: "ID should be an integer",
-        });
-    }
+    const id = event.context.params.id as string;
     return database.query.recipes.findFirst({ where: (recipe, { eq }) => eq(recipe.id, id), with: { images: true } });
 });
--- test/seedData.ts
+++ test/seedData.ts
@@ -10,37 +10,37 @@ export const seedData = {
     ],
     recipes: [
         {
-            id: 1,
+            id: "1",
             description: "desc1",
             location: "loc1",
             name: "name1",
             thumbnail: "thumbnail2",
-            user: 1,
+            user: "1",
         },
         {
-            id: 2,
+            id: "2",
             description: "desc2",
             location: "loc2",
             name: "name2",
             thumbnail: "thumbnail1",
-            user: 1,
+            user: "1",
         },
     ],
     images: [
         {
-            id: 1,
+            id: "1",
             url: "1.jpg",
-            recipe: 1,
+            recipe: "1",
         },
         {
-            id: 2,
+            id: "2",
             url: "2.jpg",
-            recipe: 1,
+            recipe: "1",
         },
         {
-            id: 3,
+            id: "3",
             url: "3.jpg",
-            recipe: 2,
+            recipe: "2",
         },
     ],
 };
