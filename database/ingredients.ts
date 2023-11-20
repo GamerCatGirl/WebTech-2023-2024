2362f770384f4cb57b9aa911e31df3889755131a
Silken <silken_heynderickx@icloud.com>
2023-11-20 08:29:34 +0100

    adding database ingredients

---
+++ database/ingredients.ts
@@ -0,0 +1,22 @@
+import { createId } from "@paralleldrive/cuid2";
+import { InferInsertModel, InferSelectModel} from "drizzle-orm";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { recipes } from "./recipe";
+
+export const ingredients = sqliteTable("ingredients", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => createId()),
+	recipyId: text("recipyId") //nog linken met andere tabel
+		.references(() => recipes.id)
+		.notNull(),
+	ingredient: text("ingredient"),
+	amount: integer("amount"),
+	type: text("type"),
+	category: text("category"),
+  });
+
+export type ingredientsDB = InferSelectModel<typeof ingredients>;
+export type InserttIngredients = InferInsertModel<typeof ingredients>;
+
+//don't understand the relatsions 
--- pages/recipies/add.vue
+++ pages/recipies/add.vue
@@ -2,48 +2,48 @@
 	<div class="card">
 		<Toolbar>
 			<template #start>
-				<Button icon="pi pi-plus" class="mr-2" />
-				<Button icon="pi pi-print" class="mr-2" />
-				<Button icon="pi pi-upload" />
-			</template>
+				<Button label="Delete" icon="pi pi-times" severity="danger" class="mr-2" />
 
-			<template #center>
-				<span class="p-input-icon-left">
-					<i class="pi pi-search" />
-					<InputText placeholder="Search" />
-				</span>
+				<!-- 	
+				<Button icon="pi pi-plus" class="mr-2" />
+				<Button icon="pi pi-pencil" class="mr-2" />
+				<Button icon="pi pi-print" class="mr-2" />
+				-->
 			</template>
 
 			<template #end>
-				<SplitButton label="Save" icon="pi pi-check" :model="items"></SplitButton>
+				<Button label="Save" icon="pi pi-check" class="mr-2" @click="save()" />
+				<Button label="Publish" icon="pi pi-upload" class="mr-2" severity="success" />
 			</template>
 		</Toolbar>
 	</div>
 
 	<div class="card flex justify-content-center">
-		<InputText placeholder="Recipy Name" />
+		<InputText placeholder="Recipy Name" v-model="recipyName" />
+		<FileUpload mode="basic" name="demo[]" url="/api/upload" accept="image/*" :maxFileSize="1000000"
+			@upload="onUpload" :auto="true" chooseLabel="Browse" />
 	</div>
 
 	<div class="card flex flex-column md:flex-row gap-3" v-for="input in inputs">
 		<!-- Name Ingredient -->
-		<InputText :placeholder="input.Ingredient" :id="input.Ingredient" />
+		<InputText placeholder="Ingredient Name" v-model="input.Ingredient" />
 
 		<!-- Amount -->
-		<InputNumber :id="input.Amount" placeholder="Amount" v-model="amount" />
+		<InputNumber v-model="input.Amount" placeholder="Amount" />
 
 		<!-- Select the type of amount -->
 		<div class="card flex justify-content-center">
-			<Dropdown v-model="amountType" editable :options="amountTypes" optionLabel="name" placeholder="Type"
-				:id="input.Type" class="w-full md:w-14rem" />
+			<Dropdown editable :options="amountTypes" optionLabel="name" placeholder="Type" v-model="input.Type"
+				class="w-full md:w-14rem" />
 		</div>
 
 		<!-- Select a category -->
 		<div class="card flex justify-content-center">
-			<Dropdown v-model="category" editable :options="categoriesIngredients" optionLabel="name"
-				:id="input.Category" :placeholder="input.Category" class="w-full md:w-14rem" />
+			<Dropdown editable :options="categoriesIngredients" optionLabel="name" v-model="input.Category"
+				placeholder="Category" class="w-full md:w-14rem" />
 		</div>
 
-		<Button icon="pi pi-times" :id="input.Counter" @click="deleteIngredient(input.Counter)" severity="danger">
+		<Button icon="pi pi-times" :v-model="input.Counter" @click="deleteIngredient(input.Counter)" severity="danger">
 		</Button>
 		<Toast />
 		<Button icon="pi pi-plus" @click="addNewRow()"> </Button>
@@ -98,19 +98,11 @@
 import { ref } from "vue";
 import { useToast } from "primevue/usetoast";
 
-const toast = useToast();
+const recipyName = ref("");
 
+const toast = useToast();
 const value = ref("");
-const items = ref([
-	{
-		label: "Update",
-		icon: "pi pi-refresh",
-	},
-	{
-		label: "Delete",
-		icon: "pi pi-times",
-	},
-]);
+const currentUser = "admin";
 
 const categoriesIngredients = ref([
 	{ name: "Vegtable" },
@@ -129,9 +121,9 @@ const amountTypes = ref([
 
 const amountType = ref();
 const dummyObject = {
-	Ingredient: "ingredient",
+	Ingredient: "",
 	Counter: "",
-	Category: "category",
+	Category: "",
 	Amount: "amount",
 	Type: "type",
 };
@@ -141,23 +133,38 @@ let amountDeleted = 0;
 const inputs = ref([newDummy()]);
 
 function newDummy() {
-	const newDummy = {
-		Ingredient: dummyObject.Ingredient + counter,
-		Counter: "" + counter,
-		Category: dummyObject.Category + counter,
-		Amount: dummyObject.Amount + counter,
-		Type: dummyObject.Type + counter,
+	return {
+		Ingredient: "",
+		Counter: counter,
+		Category: "",
+		Amount: null,
+		Type: "",
 	};
-	return newDummy;
+
+	//const newDummy.value.Ingredient = ref(),
+
+	//return newDummy;
 }
 
 function addNewRow() {
 	counter += 1;
 	inputs.value.push(newDummy());
 }
+
+const onUpload = () => {
+	toast.add({
+		severity: "info",
+		summary: "Success",
+		detail: "File Uploaded",
+		life: 3000,
+	});
+};
+
 function deleteIngredient(idString) {
 	//checken dat ref niet leeg wordt, anders error throwen met toast
 	const id = Number(idString);
+
+	//add a confirmationDialog
 
 	if (counter == amountDeleted) {
 		toast.add({
@@ -169,7 +176,84 @@ function deleteIngredient(idString) {
 	} else {
 		inputs.value.splice(id - amountDeleted, 1);
 		amountDeleted += 1;
+	}
+}
 
+function save() {
+	//to implement
+
+	if (recipyName.value == "") {
+		toast.add({
+			severity: "error",
+			summary: "Error",
+			detail: "Fill in the Recipy Name",
+			life: 3000,
+		});
+	} else {
+		inputs.value.map((value, index, array) => {
+
+			if (
+				!(
+					value.Ingredient == "" &&
+					value.Amount == null &&
+					value.Type == "" &&
+					value.Category == ""
+				)
+			) {
+				toast.add({
+					severity: "error",
+					summary: value.Ingredient,
+					detail: "lol",
+					life: 3000,
+				});
+
+				
+				}
+
+			});
+
+		/* if (value.Ingredient == "") {
+				    toast.add({
+					    severity: "error",
+					    summary: value.Ingredient,
+					    detail: "lol",
+					    life: 3000,
+				    }),
+						    } else if (value.Amount == null) {
+				    toast.add({
+					    severity: "error",
+					    summary: value.Ingredient,
+					    detail: "lol",
+					    life: 3000,
+				    }),
+						    } else if (value.Type == "") {
+				    toast.add({
+					    severity: "error",
+					    summary: value.Ingredient,
+					    detail: "lol",
+					    life: 3000,
+				    }),
+	    
+						    } else if (value.Category == "") {
+	    
+				    toast.add({
+					    severity: "error",
+					    summary: value.Ingredient,
+					    detail: "lol",
+					    life: 3000,
+				    }),
+						    } else {
+				    toast.add({
+					    severity: "error",
+					    summary: value.Ingredient,
+					    detail: "lol",
+					    life: 3000,
+				    }),
+	    
+				    //push into dataBase
+			    } */
+
+		//const recipyName = ref();
 	}
 }
 </script>
