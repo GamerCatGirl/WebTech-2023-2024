509ef2c12f5798949c9965524a5a97713d3893e6
Merlijn <github.y62fx@simplelogin.com>
2023-12-12 20:30:42 +0100

    feat: Add simple footer

--- app.vue
+++ app.vue
@@ -6,19 +6,37 @@ const theme = getTheme();
 </script>
 
 <template>
-  <div>
-    <link
-      v-if="theme === Theme.light"
-      rel="stylesheet"
-      href="/themes/soho-light/theme.css"
-    />
-    <link v-else rel="stylesheet" href="/themes/soho-dark/theme.css" />
-    <link
-      href="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css"
-      rel="stylesheet"
-    />
-    <Header />
-    <NuxtPage />
-    <Footer />
-  </div>
+    <div id="appComponent">
+        <link v-if="theme === Theme.light" rel="stylesheet" href="/themes/soho-light/theme.css" />
+        <link v-else rel="stylesheet" href="/themes/soho-dark/theme.css" />
+        <link href="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css" rel="stylesheet" />
+        <Header />
+        <NuxtPage />
+        <Footer />
+    </div>
 </template>
+
+<style>
+html,
+body {
+    --body-margin: 8px;
+    --footer-height: 78px;
+    min-height: calc(100vh - var(--body-margin) * 2);
+}
+
+#__nuxt {
+    min-height: calc(100vh - 2 * var(--body-margin));
+}
+
+#appComponent {
+    min-height: calc(100vh - 2 * var(--body-margin) - var(--footer-height));
+    position: relative;
+    margin-bottom: var(--footer-height);
+}
+
+footer {
+    position: absolute;
+    width: 100%;
+    bottom: calc(0px - var(--footer-height));
+}
+</style>
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,17 @@
 <template>
-	<footer>
-		Amazing Footer
-	</footer>
+    <footer>
+        <Menubar>
+            <template #end>
+                Made by Merlijn Devroede, Silken Heynderickx,
+                <br />
+                &emsp;&emsp;&emsp;&emsp; Elyesa Cakirbey and Pieter Verbessem</template
+            >
+        </Menubar>
+    </footer>
 </template>
+
+<style scoped>
+footer {
+    margin: 8px 0px;
+}
+</style>
