5e1443c9a05a93fbfce69739f5ec320c48f584db
Merlijn <github.y62fx@simplelogin.com>
2023-12-17 07:31:43 +0100

    remove(test): Remove tests
    This is because they don't work, and there is not enough time to
    actually fix them, or ever write more tests

--- test/API/isUp.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { setup, $fetch } from "@nuxt/test-utils";
-
-await setup({});
-test("See if API is up and reachable", async () => {
-    let isUp = await $fetch("/api/isup");
-    expect(isUp).toBeTruthy();
-});
