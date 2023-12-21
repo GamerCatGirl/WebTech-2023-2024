577af4ccef499f7cdf57775c71d178559bcb0110
Silken <silken_heynderickx@icloud.com>
2023-12-21 08:21:05 +0100

    comments server side validation

--- server/api/comments/[id]/likes/index.post.ts
+++ server/api/comments/[id]/likes/index.post.ts
@@ -1,9 +1,32 @@
-import { likedComments, InsertLikedComments } from "~/database/auth"; 
+import { likedComments, InsertLikedComments } from "~/database/auth";
 
 export default defineEventHandler(async (event) => {
-	const putLikedComment = async (likedComment: InsertLikedComments) => {
-		return database.insert(likedComments).values(likedComment);};
-		const commentToPost = await readBody(event);
-		await putLikedComment(commentToPost);
-		return true; 
-	})
+  const commentToPost = await readBody(event);
+
+  //check id comment exists
+
+    const result = await database.query.comments.findFirst({
+      where: (comment, { eq }) => eq(comment.comment, commentToPost.comment),
+      with: {
+        replies: { with: { user: { columns: { name: true, id: true } } } },
+      }})
+
+    if (!result) {
+        throw createError({
+        statusCode: 400,
+        message: "Comment to like on not found",
+      });
+    }
+  
+
+  //check id user exists
+  await authenticate(event, () => {
+    return commentToPost.user;
+  });
+
+  const putLikedComment = async (likedComment: InsertLikedComments) => {
+    return database.insert(likedComments).values(likedComment);
+  };
+  await putLikedComment(commentToPost);
+  return true;
+});
--- server/api/comments/index.post.ts
+++ server/api/comments/index.post.ts
@@ -1,14 +1,39 @@
-import {comments, InsertComment} from "~/database/recipe";
+import { comments, InsertComment } from "~/database/recipe";
 
 export default defineEventHandler(async (event) => {
-	const putComments = async (comment: InsertComment) => {
-		return database.insert(comments).values(comment);
-	};
+  const putComments = async (comment: InsertComment) => {
+    return database.insert(comments).values(comment);
+  };
 
-	const body = await readBody(event);
+  const body = await readBody(event);
 
-	body.userId = body.user;
+  //
+  await authenticate(event, () => {
+    return body.user;
+  });
 
-	await putComments(body);
-	return true;
-})
+  if (body.comment == "") {
+    throw createError({
+      statusCode: 404,
+      message: "Comment is empty",
+    });
+  }
+
+  if (body.replied) {
+    const result = await database.query.comments.findFirst({
+      where: (comment, { eq }) => eq(comment.id, body.replied),
+      with: {
+        replies: { with: { user: { columns: { name: true, id: true } } } },
+      },
+    });
+
+    if (!result){
+    	throw createError({statusCode:400, message: "Comment to reply on not found"})
+    }
+  }
+
+  body.userId = body.user;
+
+  await putComments(body);
+  return true;
+});
