33581b6a3bef0fe70841ae1efcde347152d26f53
Merlijn <github.y62fx@simplelogin.com>
2023-12-22 20:48:05 +0100

    remove(login): Google and Facebook login buttons

--- pages/login.vue
+++ pages/login.vue
@@ -25,7 +25,7 @@ const input = ref(emptyInput())
   <div class="card">
     <div class="flex flex-column md:flex-row">
       <div
-        class="w-full md:w-5 flex flex-column align-items-center justify-content-center gap-3 py-5"
+        class="w-full flex flex-column align-items-center justify-content-center gap-3 py-5"
       >
         <div
           class="flex flex-wrap justify-content-center align-items-center gap-2"
@@ -55,51 +55,17 @@ const input = ref(emptyInput())
           severity="succes"
           class="w-10rem"
           @click="() => navigateTo('/register')"
-        ></Button>
-      </div>
-
-      <div class="w-full md:w-2">
-        <Divider layout="vertical" class="hidden md:flex"><b>OR</b></Divider>
-        <Divider layout="horizontal" class="flex md:hidden" align="center"
-          ><b>OR</b></Divider
-        >
-      </div>
-      <div>
-        <p>Login/Register with</p>
-
-        <div
-          class="flex-column flex align-items-center justify-content-center gap-2"
-        >
+        />
+        <Divider layout="horizontal" class="hidden md:flex"><b>OR</b></Divider>
           <Button
             label="Github"
             icon="pi pi-github"
             severity="success"
             class="w-10rem"
             @click="signIn('github', { callbackUrl })"
-          ></Button>
-
-          <Button
-            label="Google"
-            icon="pi pi-google"
-            severity="success"
-            class="w-10rem"
-          ></Button>
-
-          <Button
-            label="Facebook"
-            icon="pi pi-facebook"
-            severity="success"
-            class="w-10rem"
-          ></Button>
-        </div>
+          />
       </div>
     </div>
   </div>
   </div>
 </template>
-
-<!--
-<style>
- @import url("~/assets/css/style.css"); 
-</style>
--->
